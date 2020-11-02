79247370d92d111004d3fc187a0f3fd052a5c648
Hooyberghs <jhooyberghs@deloitte.com>
2020-11-02 13:45:28 +0100

    #41041 Added Support for Swagger and additional documentation for the status endpoint

--- Edreams.OutlookMiddleware/Edreams.OutlookMiddleware.DataTransferObjects/Api/Base/Response.cs
+++ Edreams.OutlookMiddleware/Edreams.OutlookMiddleware.DataTransferObjects/Api/Base/Response.cs
@@ -2,8 +2,15 @@
 
 namespace Edreams.OutlookMiddleware.DataTransferObjects.Api.Base
 {
+    /// <summary>
+    /// Base class for response objects.
+    /// </summary>
     public class Response
     {
+        /// <summary>
+        /// Unique ID you can use for correlation purposes.
+        /// </summary>
+        /// <example>4ad4eb08-a9e3-4950-8a52-0ff09cfac6d8</example>
         public Guid CorrelationId { get; set; }
     }
 }
