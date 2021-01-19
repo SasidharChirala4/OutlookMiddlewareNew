d41bd86dab38b2658f875dcef1f11e2a8d8e0efc
Johnny Hooyberghs <jhooyberghs@deloitte.com>
2021-01-19 13:19:31 +0100

    #41800 Added Transaction and HistoricTransaction related entities and repositories

---
+++ Edreams.OutlookMiddleware.DataAccess/Repositories/HistoricTransactionsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Edreams.OutlookMiddleware.Common.Security.Interfaces;
+using Edreams.OutlookMiddleware.Model;
+
+namespace Edreams.OutlookMiddleware.DataAccess.Repositories
+{
+    public class HistoricTransactionsRepository : Repository<HistoricTransaction>
+    {
+        public HistoricTransactionsRepository(
+            OutlookMiddlewareDbContext dbContext, ISecurityContext securityContext)
+            : base(dbContext, dbContext.TransactionHistory, securityContext)
+        {
+
+        }
+    }
+}
---
+++ Edreams.OutlookMiddleware.Model/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Edreams.OutlookMiddleware.Model.Base;
+using Edreams.OutlookMiddleware.Model.Enums;
+
+namespace Edreams.OutlookMiddleware.Model
+{
+    public class Transaction : ModelBase, ILongSysId
+    {
+        public Guid BatchId { get; set; }
+
+        public TransactionStatus Status { get; set; }
+
+        public DateTime? Scheduled { get; set; }
+
+        public DateTime? ProcessingStarted { get; set; }
+
+        public DateTime? ProcessingFinished { get; set; }
+
+        public string ProcessingEngine { get; set; }
+
+        public Guid CorrelationId { get; set; }
+    }
+}
