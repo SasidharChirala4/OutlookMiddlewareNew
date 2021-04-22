46ab49fa4aceed0d0cd6bfac4b735d7754180499
Kiran Mohan Karedla <kkaredla@deloitte.com>
2021-04-22 16:14:19 +0530

    added internetmessageid

--- Edreams.OutlookMiddleware.DataAccess/OutlookMiddlewarePreloadDbContext.cs
+++ Edreams.OutlookMiddleware.DataAccess/OutlookMiddlewarePreloadDbContext.cs
@@ -50,6 +50,15 @@ namespace Edreams.OutlookMiddleware.DataAccess
                 e
                     .Property(x => x.FileStatus)
                     .HasConversion(new EnumToStringConverter<FilePreloadStatus>());
+                e.
+                     Property(x => x.InternetMessageId)
+                    .HasMaxLength(200);
+                e.
+                     Property(x => x.InsertedBy).IsRequired();
+                e.
+                     Property(x => x.InsertedBy).HasMaxLength(100);
+                e.
+                     Property(x => x.UpdatedBy).HasMaxLength(100);
 
             });
         }
